eb1c656b475f157317015db9265b32e234d8ff8d
yuanjin <yuanjinmoke@gmail.com>
2019-07-02 14:41:14 +0800

    10-9

--- plugin/helpers.js
+++ plugin/helpers.js
@@ -117,3 +117,26 @@ this.myPlugin.throttle = function (callback, time, immediately) {
         }
     }
 }
+
+/**
+ * 科里化函数
+ * 在函数式编程中，科里化最重要的作用是把多参函数变为单参函数
+ */
+this.myPlugin.curry = function (func) {
+    //得到从下标1开始的参数
+    var args = Array.prototype.slice.call(arguments, 1);
+    var that = this;
+    return function () {
+        var curArgs = Array.from(arguments); //当前调用的参数
+        var totalArgs = args.concat(curArgs);
+        if (totalArgs.length >= func.length) {
+            //参数数量够了
+            return func.apply(null, totalArgs);
+        }
+        else {
+            //参数数量仍然不够
+            totalArgs.unshift(func);
+            return that.curry.apply(that, totalArgs);
+        }
+    }
+}
