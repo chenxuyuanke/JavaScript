44a6717c3a1217c371e00d7d26110fca538ff114
yuanjin <yuanjinmoke@gmail.com>
2019-07-02 15:19:39 +0800

    10-10

--- plugin/helpers.js
+++ plugin/helpers.js
@@ -140,3 +140,19 @@ this.myPlugin.curry = function (func) {
         }
     }
 }
+/**
+ * 函数管道
+ */
+this.myPlugin.pipe = function () {
+    var args = Array.from(arguments);
+    return function (val) {
+        return args.reduce(function (result, func) {
+            return func(result);
+        }, val);
+        // for (var i = 0; i < args.length; i++) {
+        //     var func = args[i];
+        //     val = func(val);
+        // }
+        // return val;
+    }
+}   
